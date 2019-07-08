e26cd6c590b1e100c7e14ede2b98161ae8496a1e
ksugawara61 <katsuya@kokohadoko.net>
2019-07-09 00:28:44 +0900

    [update] prevent error message if directory is empty

--- bin/after_up_proxy.sh
+++ bin/after_up_proxy.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+SYNC_DIR='./dev'
+TMP_DIR='../tmp-dev'
+
 # devディレクトリのファイルを戻す
 echo 'start recovery dev directory...'
-mv ../tmp-dev/* dev/
+if [ "$(ls $TMP_DIR)" ]; then
+  mv $TMP_DIR/* $SYNC_DIR
+fi
 echo 'recover files are as follows:'
-ls -l dev/ | grep -v total
-rmdir ../tmp-dev/
+ls -l $SYNC_DIR
+rmdir $TMP_DIR
 echo 'end recovery dev directory.'
